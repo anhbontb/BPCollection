cc8bb552d44ddf0436b5349499bec08b511f68b3
Bon <anhbontb@gmail.com>
2018-02-23 21:27:19 +0700

    [Project] init

---
+++ BPCollection/BPCollection/BPCollection.h
@@ -0,0 +1,19 @@
+//
+//  BPCollection.h
+//  BPCollection
+//
+//  Created by Bon Bon on 2/23/18.
+//  Copyright © 2018 Bon Bon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BPCollection.
+FOUNDATION_EXPORT double BPCollectionVersionNumber;
+
+//! Project version string for BPCollection.
+FOUNDATION_EXPORT const unsigned char BPCollectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BPCollection/PublicHeader.h>
+
+
