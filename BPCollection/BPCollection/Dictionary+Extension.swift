45153fcc88656a96ac91ab51316ffc6bdfdfcc47
Bon <anhbontb@gmail.com>
2018-02-24 08:54:20 +0700

    [Dictionary] public function

--- BPCollection/BPCollection/Dictionary+Extension.swift
+++ BPCollection/BPCollection/Dictionary+Extension.swift
@@ -11,7 +11,7 @@ import Foundation
 //MARK: - Dictionary
 
 extension Dictionary where Value == Any {
-    func value<T>(forKey key: Key, defaultValue: @autoclosure () -> T) -> T {
+    public func value<T>(forKey key: Key, defaultValue: @autoclosure () -> T) -> T {
         guard let value = self[key] as? T else {
             return defaultValue()
         }
